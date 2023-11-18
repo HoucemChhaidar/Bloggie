f15f70c16ce15df85d0d42b7ef4a1d7d6170f200
HoucemChhaidar <houcem.chhaidar@gmail.com>
2023-11-18 22:44:10 +0100

    Run EF core migrations

--- Bloggie/Program.cs
+++ Bloggie/Program.cs
@@ -6,8 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BloggieDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbContext")));
-
+options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbConnectionString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
